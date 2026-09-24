d4708b8a604e372f2bc87b73dbd3e98f390fc2da
ellcode655 <gededarell2@gmail.com>
2026-09-24 14:17:21 +0800

    n4

--- script.js
+++ script.js
@@ -2,24 +2,24 @@ const carousel = document.querySelector("#carouselExampleInterval");
 const items = carousel.querySelectorAll(".carousel-item");
 
 function updateSideSlides() {
-  const active = carousel.querySelector(".carousel-item.active");
+    const active = carousel.querySelector(".carousel-item.active");
 
-  const activeIndex = Array.from(items).indexOf(active);
+    const activeIndex = Array.from(items).indexOf(active);
 
-  const previousIndex = (activeIndex - 1 + items.length) % items.length;
+    const previousIndex = (activeIndex - 1 + items.length) % items.length;
 
-  const nextIndex = (activeIndex + 1) % items.length;
+    const nextIndex = (activeIndex + 1) % items.length;
 
-  // Remove old classes
-  items.forEach((item) => {
-    item.classList.remove("prev-slide", "next-slide");
-  });
+    // Remove old classes
+    items.forEach((item) => {
+        item.classList.remove("prev-slide", "next-slide");
+    });
 
-  // Add previous
-  items[previousIndex].classList.add("prev-slide");
+    // Add previous
+    items[previousIndex].classList.add("prev-slide");
 
-  // Add next
-  items[nextIndex].classList.add("next-slide");
+    // Add next
+    items[nextIndex].classList.add("next-slide");
 }
 
 // Initial setup
@@ -27,5 +27,5 @@ updateSideSlides();
 
 // Update after Bootstrap finishes changing slide
 carousel.addEventListener("slid.bs.carousel", function () {
-  updateSideSlides();
+    updateSideSlides();
 });
